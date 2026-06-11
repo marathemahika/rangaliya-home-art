3b8ec5ee8510c5b40f734bf6cfe26dc85f324b01
Mahika <marathemahika11@gmail.com>
2026-06-11 15:00:35 +0530

    push

--- src/components/LogoAnimation.tsx
+++ src/components/LogoAnimation.tsx
@@ -55,7 +55,7 @@ const LogoAnimation = () => {
           y: 0,
         }}
         transition={{
-          duration: 2,
+          duration: 2.5,
           ease: [0.22, 1, 0.36, 1],
         }}
         style={{ height: "300px" }}
